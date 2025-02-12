009ebde85be96760f28ce1a87a97e1a69a738d61
chunchuAkhil <akhilchunchu52@gmail.com>
2025-02-12 17:42:26 +0530

    defined routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,10 @@
-const protectedRoutes = {
-  gotoProfilePage: "/auth/profile",
-};
 export const appRouter = {
+  //public routes
   gotoLandingPage: "/Landing",
   gotoBikeDetailsPage: "/details",
   gotoLoginPage: "/login",
-  gotoProfilePage: protectedRoutes.gotoProfilePage,
+  //protected routes
+  gotoVerificationPage: "/auth/verification",
+  gotoPaymentPage: "/auth/payment",
+  gotoProfilePage: "/auth/profile",
 };
