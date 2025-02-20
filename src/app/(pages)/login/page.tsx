7481ccc21629bcb12fb430782bf1447b6edd0164
chunchuAkhil <akhilchunchu52@gmail.com>
2025-02-20 19:40:00 +0530

    added protected routes

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -1,12 +1,9 @@
 import GoogleLogin from "@/view/auth/GoogleLogin";
-import Login from "@/view/auth/Login";
 import React from "react";
 
 const page = () => {
   return (
     <div>
-      Fake Login page
-      <Login />
       <GoogleLogin />
     </div>
   );
